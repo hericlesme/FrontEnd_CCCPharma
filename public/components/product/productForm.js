67d8b3ed886cef497ba9f78e5648667eb1c7d538
Héricles Emanuel <hericles.me@gmail.com>
2018-12-11 17:20:18 -0300

    form style

--- public/components/product/productForm.js
+++ public/components/product/productForm.js
@@ -27,14 +27,13 @@ class ProductForm extends HTMLFormElement {
 	        <h1>Formulário de cadastro de produtos</h1>
 			<form class="product_form">
 				<div>
-					<div>
-						<p> Nome: <input type="text" name="nome" class="field" autocomplete="off" required> </p>
-						<p> Categoria: <input type="text" name="categoria" class="field" autocomplete="off" required> </p>
-						<p> Código de Barra: <input type="text" name="codigo" class="field" autocomplete="off" required> </p>
-						<p> Fabricante: <input type="text" name="fabricante" class="field" autocomplete="off" required> </p>
-					</div>
-					<button type="submit">Enviar</button>
+					<label for="nome"> Nome: </label> 
+					<input type="text" id="nome" name="nome" class="field" autocomplete="off" required>
+					<label for="categoria"> Categoria: </label> <input type="text" id="categoria" name="categoria" class="field" autocomplete="off" required>
+					<label for="codigo"> Código de Barra: </label> <input type="text" id="codigo" name="codigo" class="field" autocomplete="off" required>
+					<label for="fabricante"> Fabricante: </label> <input type="text" id="fabricante" name="fabricante" class="field" autocomplete="off" required>
 				</div>
+				<button type="submit">Enviar</button>
 			</form>
 		`
     }
