5eaafb8236d706b7bf62bcf83097469138503eae
Héricles <hericles.me@gmail.com>
2018-12-13 15:18:59 -0300

    add edited components

---
+++ components/product/Product.js
@@ -0,0 +1,45 @@
+class Product extends HTMLLIElement {
+    /**
+    * Construtor de Product
+    * 
+    * @param product Object com os atributos de Product
+    */
+    constructor(product) {
+        super();
+        for (const attribute in product) {
+            this.setAttribute(attribute, product[attribute]);
+        }
+    }
+
+    connectedCallback() {
+        this.imageUrl = this.getAttribute('imageUrl');
+        this.category = this.getAttribute('category');
+        this.price = this.getAttribute('price');
+        this.name = this.getAttribute('name');
+        this.render();
+    }
+
+    render() {
+        let classes = ["mdc-image-list__item", "product"];
+        this.classList.add(...classes);
+
+        this.innerHTML = `
+        <h2>${this.name.toUpperCase()}</h2>
+        <img class="mdc-image-list__image" src="${this.imageUrl}">
+        <div class="mdc-image-list__supporting">
+            <p>${this.category.toUpperCase()}</p>
+            <span>R$ ${this.price}</span>
+        </div>
+    `
+    }
+}
+
+try {
+    customElements.define('product-card', Product, {extends: 'li'});
+} catch (err) {
+    const h3 = document.createElement('h3');
+    h3.innerHTML = "Something went wrong!";
+    document.body.appendChild(h3);
+}
+
+export { Product };
---
+++ components/product/ProductList.js
@@ -0,0 +1,37 @@
+import {Product} from './Product.js';
+
+class ProductList extends HTMLUListElement {
+    getProductsJson() {
+        // WORK IN PROGRESS
+        return fetch(this.url + "/products.json", {method: "GET"})
+            .then(data => data.json())
+            .catch(err => console.log(err.message));
+    }
+
+    setProductsInDOM(productList) {
+        productList.forEach(product => {
+            const productCard = new Product(product);
+            this.appendChild(productCard);
+        });
+    }
+
+    connectedCallback(){
+        this.url = "components/product"
+        this.getProductsJson().then(productsJson => this.render(productsJson));
+    }
+
+    render(products){
+        var classes = ['mdc-image-list', 'mdc-image-list--masonry', 'product-list'];
+        this.classList.add(...classes);
+        this.setProductsInDOM(products);
+    }
+}
+
+try {
+    customElements.define('product-list', ProductList, {extends: 'ul'});
+    
+} catch (err) {
+    const h3 = document.createElement('h3');
+    h3.innerHTML = "Something went wrong!";
+    document.body.appendChild(h3);
+}
