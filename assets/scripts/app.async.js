68f1ee431cad3f770051a681bc57d5e6ce7576c1
Matheus Oliveira <matheus7op@gmail.com>
2018-12-17 00:46:03 -0300

    Testing

--- assets/scripts/app.async.js
+++ assets/scripts/app.async.js
@@ -1,13 +1,13 @@
 import { MDCDialog } from '@material/dialog';
 import { register } from './app.js'
 
-setTimeout(() => {
-	register();
-
-	let $submitProductButton = document.querySelector("#submit-new-product");
-	$submitProductButton.addEventListener("click", function () {
-		setTimeout(() => {
-			register();
-		}, 1000);
-	});
-}, 1000);
+//setTimeout(() => {
+//	register();
+//
+//	let $submitProductButton = document.querySelector("#submit-new-product");
+//	$submitProductButton.addEventListener("click", function () {
+//		setTimeout(() => {
+//			register();
+//		}, 1000);
+//	});
+//}, 1000);
